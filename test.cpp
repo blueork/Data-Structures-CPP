8d8e9946f09ce58485e3b19b1a6159f61ddfef58
blueork <114741623+blueork@users.noreply.github.com>
2023-12-23 18:42:16 +0500

    Update completeBT.h
    Rename BinaryTreeNode class to CBT. Set "Node* root" as a private data
    member.

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include ".\CompleteBT\completeBT.h"
+using namespace std;
+
+int main() {
+
+  CBT alpha;
+
+  alpha.insertNode(10);
+  alpha.insertNode(10);
+  alpha.insertNode(10);
+  alpha.insertNode(10);
+  alpha.insertNode(10);
+  alpha.display();
+  
+  return 0;
+}
+
+
