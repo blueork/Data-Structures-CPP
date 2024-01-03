b4210aa4f7c37637f83b15de45b602e4f60b5547
blueork <114741623+blueork@users.noreply.github.com>
2024-01-03 19:56:02 +0500

    Update avl.h
    Add relevant comments to both of the classes AVLNode and AVLTree

--- AVLTree/avl.h
+++ AVLTree/avl.h
@@ -6,12 +6,14 @@ class AVLNode {
 public:
 	int data, height;
 	AVLNode* left, * right;
+	// defualt constructor
 	AVLNode( int data = 0, AVLNode* left = NULL, AVLNode* right = NULL, int height = 0) {
 		this->data = data;
 		this->left = left;
 		this->right = right;
 		this->height = height;
 	}
+	// destructor
 	~AVLNode() {
 		this->left = NULL;
 		this->right = NULL;
@@ -22,6 +24,7 @@ public:
 class AVLTree {
 private:
 	AVLNode* root;
+	// inserts the given data in its correct place in the AVL Tree
 	void insert(AVLNode*& current, int& data) {
 		if (current == NULL)			
 			return;
@@ -39,6 +42,7 @@ private:
 			else
 				this->insert(current->right, data);	// if right child is non-empty
 		}
+		// rebalance the AVL Tree after insertion when backtracking
 		current->height = findNodeHeight(current);
 		this->rebalancing(current);
 	}
@@ -113,6 +117,7 @@ private:
 		this->RRrotation(root);
 		// performing LL rotation on the resultant sub-tree
 	}
+	// display the AVL Tree in inorder
 	void inorder(AVLNode* current) {
 		if (current == NULL)
 			return;
@@ -120,6 +125,7 @@ private:
 		cout << current->data << " ";
 		inorder(current->right);
 	}
+	// display the AVL Tree in preorder
 	void preorder(AVLNode* current) {
 		if (current == NULL)
 			return;
@@ -127,6 +133,7 @@ private:
 		preorder(current->left);
 		preorder(current->right);
 	}
+	// display the AVL Tree in postorder
 	void postorder(AVLNode* current) {
 		if (current == NULL)
 			return;
@@ -134,15 +141,19 @@ private:
 		postorder(current->right);
 		cout << current->data;
 	}
+	// finds and deletes the AVLNode to be deleted 
+	// performs inorder successor deletion
 	void remove(AVLNode*& current, int& data) {
 		if (current == NULL)
 			return;
 		if (current->data == data) {
 			int degree = this->degree(current);
+			// if the AVLNode to be deleted has no children
 			if (degree == 0) {
 				delete current;
 				current = NULL;
 			}
+			// if the AVLNode to be deleted has one child
 			else if (degree == 1) {
 				AVLNode* temp = current;
 				if (current->left == NULL)
@@ -152,9 +163,14 @@ private:
 				delete temp;
 				temp = NULL;
 			}
+			// if the AVLNode to be deleted has both the children
 			else {
 				AVLNode* temp = current;
+				// find the inorder successor
 				current = findMinNode(current->right);
+				// adjust the AVL Tree
+				// the left and right subtrees of the AVL Node to deleted
+				// become the left and right subtrees of the inorder successor
 				current->left = temp->left;
 				current->right = temp->right;
 				delete temp;
@@ -165,9 +181,9 @@ private:
 			this->remove(current->right, data);
 		else
 			this->remove(current->left, data);
+		// adjust AVL Tree after deletion when backtracking
 		current->height = findNodeHeight(current);
 		this->rebalancing(current);
-
  	}
 	AVLNode* findMinNode(AVLNode*& current) { // finds the node with the minimum value in the given sub-tree
 		AVLNode* temp = NULL;										// further replaces the node with its right child or NULL and	
@@ -182,6 +198,7 @@ private:
 		}
 		return temp;		
 	}
+	// returns the minimum value in the tree
 	int findMinValue(AVLNode*& current) {
 		if (current == NULL)
 			return -1;
@@ -189,6 +206,7 @@ private:
 			return current->data;
 		return findMinValue(current->left);
 	}
+	// returns the maximum value in the tree
 	int findMaxValue(AVLNode*& current) {
 		if (current == NULL)
 			return -1;
@@ -197,17 +215,22 @@ private:
 		return findMaxValue(current->right);
 	}
 public:
+	// defualt constructor
 	AVLTree(AVLNode* root = NULL) {
 		this->root = root;
 	}
+	// destructor
 	~AVLTree() {
 		this->root = NULL;
 	}
+	// inserts the given data into the tree
 	void insert(int data) {
+		// if the tree is empty
 		if (this->root == NULL)
 			this->root = new AVLNode(data);
 		this->insert(root, data);
 	}
+	// returns the height of the provided AVLNode
 	int findNodeHeight(AVLNode*& current) {
 		if (current == NULL)
 			return -1;
@@ -216,9 +239,10 @@ public:
 			leftHeight = current->left->height;	// find the height of the left child subtree
 		if (current->right)							// if right child exists
 			rightHeight = current->right->height;// find the height of the right child subtree
-		return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
 		// returns the height of the subtree with greater height
+		return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
 	}
+	// returns the balance factor of the provided AVLNode
 	int balanceFactor(AVLNode* current) {
 		if (current == NULL)
 			return -1;
@@ -227,45 +251,61 @@ public:
 			leftHeight = current->left->height;
 		if (current->right)
 			rightHeight = current->right->height;
+		// returns height of left subtree - height subtree 
 		return leftHeight - rightHeight;
 	}
+	// returns the balance factor of the provided AVLNode
 	int getBalanceFactor(AVLNode* root) {
 		return balanceFactor(root);
 	}
+	// checks for rebalancing at given AVLNode and performs the
+	// requried rotations
 	void rebalancing(AVLNode*& current) {
+		// left subtree is left heavy
 		if (this->balanceFactor(current) == 2 && this->balanceFactor(current->left) == 1)
 			this->LLrotation(current);
+		// right subtree is right heavy
 		else if (this->balanceFactor(current) == -2 && this->balanceFactor(current->right) == -1)
 			this->RRrotation(current);
+		// left subtree is right heavy
 		else if (this->balanceFactor(current) == 2 && this->balanceFactor(current->left) == -1)
 			this->LRrotation(current);
+		// right subtree is left heavy
 		else if (this->balanceFactor(current) == -2 && this->balanceFactor(current->right) == 1)
 			this->RLrotation(current);
 	}
+	// displays the tree in inorder
 	void display() {
 		cout << "In-order traversal: ";
 		this->inorder(root);
 		cout << "\n";
 	}
+	// displays the tree in inorder
 	void inOrderDisplay() {
 		this->display();
 	}
+	// displays the tree in preorder
 	void preOrderDisplay() {
 		cout << "Pre-order traversal: ";
 		this->preorder(root);
 		cout << "\n";
 	}
+	// displays the tree in postorder
 	void postOrderDisplay() {
 		cout << "Post-order traversal: ";
 		this->postorder(root);
 		cout << "\n";
 	}
+	// removes the given data from the tree is found
 	void remove(int data) {
+		// tree is empty
 		if (this->root == NULL)
 			return;
 		else
 			this->remove(this->root, data);
 	}
+	// returns the degree i.e. the number of children
+	// of the given AVLNode
 	int degree(AVLNode*& current) {
 		if (current == NULL)		
 			return -1;
@@ -278,37 +318,51 @@ public:
 		else				// if it has two childs
 			return 2;
 	}
+	// returns the minimum value present in the tree
 	int findMinValue() {
 		if (this->root == NULL)
 			return -1;
 		this->findMinValue(this->root);
 	}
+	// returns the maximum value present in the tree
 	int findMaxValue() {
 		if (this->root == NULL)
 			return -1;
 		this->findMaxValue(this->root);
 	}
+	// checks whether a provided tree is AVL or not
 	bool verifyAVLTree(AVLNode*& root) {
 		if (root == NULL)
 			return true;
+		// recursive call
+		// check whether the left subtree is AVL or not
 		if (verifyAVLTree(root->left) == false)
 			return false;
+		// recursive call
+		// check whether the right subtree is AVL or not
 		if (verifyAVLTree(root->right) == false)
 			return false;
+		// update height of the current AVLNode	
 		root->height = this->findNodeHeight(root);
+		// get the balance of the current AVLNode
 		int val = this->balanceFactor(root);
+		// ensures that the current AVLNode satisfies the properties of an AVL Tree
+		// ensures the balance factor is within the specified range of an AVL Tree
 		if (val == -1 || val == 0 || val == 1)
 			return true;
 		else
 			return false;
 	}
+	// splits the tree into two provided AVLTrees based on the threshold value
+	// if the value is less or equal to the threshold value it is inserted into
+	// the provided treeA otherwise inserted into the provided treeB
 	void splitTree(int threshold, AVLTree& treeA, AVLTree& treeB) {
 		if (this->root == NULL)
 			return;
 		inorderValuesInsert(threshold, treeA, treeB, this->root);
 	}
-	// traverses the tree in inorder manner and copies the values into
-	// treeA and treeB according to the threshold
+	// traverses the tree in inorder manner and copies the values
+	// into the trees treeA and treeB according to the threshold
 	void inorderValuesInsert(int& threshold, AVLTree& treeA, AVLTree& treeB, AVLNode*& current) {
 		if (current == NULL)
 			return;
@@ -319,6 +373,7 @@ public:
 			treeB.insert(current->data);
 		inorderValuesInsert(threshold, treeA, treeB, current->right);
 	}
+	// returns the root of the AVL Tree
 	AVLNode* getRoot() {
 		return this->root;
 	}
